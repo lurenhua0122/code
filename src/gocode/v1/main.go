93ebab610315b6eaf51b2dbb2fd9d6cc7f8d75cf
renhualu@aliyun.com <1472638@qq.com>
2022-05-14 16:23:47 +0800

    init
    init

---
+++ src/gocode/v1/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// 定义结构
+// 前区块哈希
+// 当前区块哈希
+// 数据
+
+// 创建区块
+// 生成哈希
+// 引入区块链
+// 添加区块
+// 重构代码
+
+type Block struct {
+	PrevBlockHash []byte
+	Hash          []byte
+	Data          []byte
+}
+
+func main() {
+	fmt.Printf("helloworld\n")
+}
